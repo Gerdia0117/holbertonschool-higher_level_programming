ca8d389bd29ff0cd052775cc90f07aac188e20da
Gerdia0117 <Gerald.froz@gmail.com>
2025-05-18 20:58:16 -0400

    commiting

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,6 +4,8 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
+
+
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     # Test with valid indices
