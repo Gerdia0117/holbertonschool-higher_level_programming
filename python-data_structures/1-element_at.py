c7de97fd1dfb77191efebfba7875e7cf214a869a
Gerdia0117 <Gerald.froz@gmail.com>
2025-05-18 20:56:59 -0400

    trailing white space

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,7 +4,6 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
-
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     # Test with valid indices
