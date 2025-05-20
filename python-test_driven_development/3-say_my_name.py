fc907ce87b7f3aea68be2493ab1939da97b1195a
Gerdia0117 <Gerald.froz@gmail.com>
2025-05-20 13:12:37 -0400

    fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -41,5 +41,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     name = "{}{}".format(first_name, " " + last_name if last_name else " ")
-    print("My name is {}".format(name))
-    
