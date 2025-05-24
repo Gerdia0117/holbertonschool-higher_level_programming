2ee7e6c5ae42b8c95de00fe64d374ab822e1a65c
Gerdia0117 <Gerald.froz@gmail.com>
2025-05-24 11:57:46 -0400

    wrote a class square that defines a square based on file 0

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a square with size."""
+        self.__size = size
