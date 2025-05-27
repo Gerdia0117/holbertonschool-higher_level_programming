b1637d86cb85982f2524380d6d8a2dd94fdb476f
Gerdia0117 <Gerald.froz@gmail.com>
2025-05-27 14:43:05 -0400

    had issue with shbang

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,17 +1,16 @@
-#1/user/bin/python3
-"""Check if objects is instance of/inherited from class."""
+#!/usr/bin/python3
+"""Check if object is instance of/inherited from class."""
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if object isinstance of or inherited from class
+    """Check if object is instance of or inherited from class.
 
     Args:
-        obj: object to check
-        a_class: Calss to compare against
+        obj: Object to check
+        a_class: Class to compare against
 
     Returns:
         bool: True if obj is instance of or inherited from a_class,
               False otherwise
-
-"""
-return isinstance(obj, a_class)
+    """
+    return isinstance(obj, a_class)
