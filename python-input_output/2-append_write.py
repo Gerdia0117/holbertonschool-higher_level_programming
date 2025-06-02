81a2be31b4cd1a1cabeff58866a3bc5da4305074
Gerdia0117 <Gerald.froz@gmail.com>
2025-06-02 07:16:07 -0400

    function grabs a text at the end of file and returns number of characters added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for appending a string to a text file and returning character count.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to a text file (UTF8) and returns number of characters added.
+
+    Args:
+        filename (str): Name of file to append to
+        text (str): Text to append to file
+
+    Returns:
+        int: Number of characters added
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
