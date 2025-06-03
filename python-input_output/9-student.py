b41c9060df855661abb6632a15659e522054f468
Gerdia0117 <Gerald.froz@gmail.com>
2025-06-03 11:05:32 -0400

    class Student that defines a student by name, last name, age

---
+++ python-input_output/9-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module for defining a Student class with JSON serialization capability.
+"""
+
+
+class Student:
+    """
+    A class that represents a student with basic information and JSON conversion.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): Student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary containing the student's attributes
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
