a1eababbaf99c789325bf4aa70662c7f67206e2d
Gerdia0117 <Gerald.froz@gmail.com>
2025-09-03 13:57:14 -0400

    fixing a problem that involves the checkers

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-str = ("Python is an interpreted, interactive, object-oriented "
-       "programming language that combines remarkable power with "
-       "very clear syntax")
-print(str[39:67] + str[107:112] + str[:6])
+print(__import__('this').s.encode('utf-8').decode('utf-8')[37:65] + " with Python")
