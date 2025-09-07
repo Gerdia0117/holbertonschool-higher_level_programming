e67daed33fc70c458f42d0da5ceb285606a272ab
Gerdia0117 <Gerald.froz@gmail.com>
2025-09-07 19:06:52 -0400

    correcting checker error

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    """Print a string in uppercase followed by a new line"""
-    for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            c = chr(ord(c) - 32)  # convert to uppercase
-        print("{}".format(c), end="")
-    print()
+def print_last_digit(number):
+    """Prints the last digit of a number and returns it"""
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
