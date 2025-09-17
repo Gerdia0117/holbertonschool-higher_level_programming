80f8ceb2f31e5ea531ccd7a3068233868a52be6c
Gerdia0117 <Gerald.froz@gmail.com>
2025-09-17 18:06:39 -0400

    Task 1 square size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+
+
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            Size: size of the square (no type/value verification)
+        """
+        self.__size = size
