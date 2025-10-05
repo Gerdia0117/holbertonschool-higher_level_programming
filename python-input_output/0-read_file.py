551c69ec4d8361d033e4a239a1a719a4e06f6a89
Gerdia0117 <Gerald.froz@gmail.com>
2025-10-05 14:22:31 -0400

    changed foulders name, it had spelling error

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for reading and printing text files"""
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its contents to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
